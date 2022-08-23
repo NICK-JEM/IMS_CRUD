f1cb56d1723582ede93637266f2d79794849f8e9
Jack <failee6@hotmail.co.uk>
2022-08-23 10:58:22 +0100

    added update and delete functions

--- src/main/java/com/qa/unnamedcrud/operations/CRUDoperations.java
+++ src/main/java/com/qa/unnamedcrud/operations/CRUDoperations.java
@@ -17,6 +17,10 @@ public class CRUDoperations {
 	private Statement stmt;  //query statement for SQL 
 	private ResultSet rs; 	 //results returned from SQL db
 	
+	public String forTest;
+	public String deleted;
+	public String updated;
+	
 	public CRUDoperations() {
 		//attempt to open a connection using credentials stored in DBconfig
 		try {
@@ -24,26 +28,30 @@ public class CRUDoperations {
 			conn = DriverManager.getConnection(DBconfig.getUrl(), DBconfig.getUser(), DBconfig.getPass());
 			this.stmt = conn.createStatement();
 			System.out.println("connection successful");
+			forTest = "Okay";
 		} catch(SQLException e) {
 			System.out.println("incorrect credentials");
 			e.printStackTrace();
+			forTest = "NotOkay";
 		}
 		
 	}
 	
-	public void create(Equipment item) {
+	public Equipment create(Equipment item) {
 		String createItem = "INSERT INTO equipment(item_name, item_desc, dept, quantity, stored_by, store_date) VALUES('"+item.getItem_name()+"', '"+item.getItem_desc()+"' ,'"+item.getDept()+"',"+item.getQuantity()+", '"+item.getStored_by()+"','"+item.getDate()+"');"; 
 		
 		try {
 			stmt.executeUpdate(createItem);
 			System.out.println("new item added to labs database");
+			return item;
 		} catch(SQLException e) {
 			System.out.println("bad query");
 			e.printStackTrace();
+			return item;
 		}
 	}
 	
-	public void read() {
+	public ResultSet read() {
 		String readData = "SELECT*FROM equipment;";
 		try {
 			rs = stmt.executeQuery(readData);
@@ -59,11 +67,46 @@ public class CRUDoperations {
 				
 			}
 			
+			return rs;
+			
 		} catch(SQLException e) {
 			System.out.println("Bad query");
 			e.printStackTrace();
 			
+			return rs;
 		}
+	}
+	
+	public void update(int item_id, String upItemName, String upItemDesc, String upDept, int upQuant, String upByWhom, String upStoreDate) {
+		
+		String upStmt = "UPDATE equipment SET item_name = '"+upItemName+"', item_desc = '"+upItemDesc+"', dept = '"+upDept+"', quantity = "+upQuant+", stored_by = '"+upByWhom+"', store_date = '"+upStoreDate+"' WHERE item_id = "+item_id+"; ";
+		
+		try {
+			stmt.executeUpdate(upStmt);
+			updated = "update successful";
+			System.out.println(updated);
+		} catch(SQLException e){
+			updated = "bad query";
+			System.out.println(updated);
+			e.printStackTrace();
+			
+		}
+		
+	}
+	
+	public void delete(int item_id) {
+		String delStmt = "DELETE FROM equipment WHERE item_id="+ item_id+";";
+		
+		try {
+			stmt.executeUpdate(delStmt);
+			deleted = "record successfully removed from database.";
+			System.out.println(deleted);
+		} catch(SQLException e) {
+			deleted = "bad query";
+			System.out.println(deleted);
+			e.printStackTrace();
+		}
+		
 	}
 	
 	
